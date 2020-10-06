6311e806fc21924c241f719c4ce7ae2338f644f1
TheOnlyAesir <71496688+TheOnlyAesir@users.noreply.github.com>
2020-10-06 22:01:47 +0200

    Initial commit

---
+++ src/main/java/rest/onlinednd/OnlinedndApplication.java
@@ -0,0 +1,13 @@
+package rest.onlinednd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinedndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinedndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rest/onlinednd/OnlinedndApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.onlinednd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinedndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
